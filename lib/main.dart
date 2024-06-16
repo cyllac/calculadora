cee2f2405cf3b1f75d7f2decef85fc9ed9a81d80
Priscila <cyllac@gmail.com>
2024-06-16 10:05:55 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Center(child: Text('Calculadora')),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Text('Coluna 1'),
+            Text('Coluna 2'),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Text('7'),
+                Text('8'),
+                Text('9'),
+                Text('/'),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Text('4'),
+                Text('5'),
+                Text('6'),
+                Text('*'),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Text('1'),
+                Text('2'),
+                Text('3'),
+                Text('-'),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Text('0'),
+                Text(','),
+                Text('='),
+                Text('+'),
+              ],
+            ),
+            Text('Coluna 6'),
+          ],
+        ),
+      ),
+    );
+  }
+}
